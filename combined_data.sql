7143bb7b5b3a5fae6f879dc61b21e4a3cf724516
Aidan Casey <caseya@moravian.edu>
2025-11-25 11:13:46 -0500

    Updated sql combination to include police data

--- combined_data.sql
+++ combined_data.sql
@@ -3,12 +3,13 @@ DROP TABLE IF EXISTS combined_data;
 CREATE TABLE combined_data AS
     -- Los Angeles
     SELECT
-        "Date Rptd"::DATE as date_occ,
+        "DATE OCC"::DATE as date_occ,
         "Location" as location,
-        "Crm Cd" as nibrs_code,
+        nibrs_code,
         "Crm Cd Desc" as description,
-        LAT,
-        LON
+        CLOSEST_STATION,
+        CLOSEST_STATION_OBJECTID,
+        DISTANCE_METERS
     FROM read_csv_auto('los_angeles_clean.csv')
     WHERE YEAR("Date Rptd"::DATE) = 2020  -- Filter for LA
 
@@ -20,9 +21,10 @@ UNION ALL
         "Block" as location,
         nibrs_code,
         Description as description,
-        latitude as LAT,
-        longitude as LON,
-    FROM read_csv_auto('chicago_clean.csv')
+        CLOSEST_STATION_NAME as CLOSEST_STATION,
+        CLOSEST_STATION_OBJECTID,
+        DISTANCE_METERS
+    FROM read_csv_auto('Chicago_clean.csv')
     WHERE YEAR("Date"::DATE) = 2020       -- Filter for Chicago
 
 UNION ALL
@@ -33,7 +35,8 @@ UNION ALL
         location_block as location,
         nibrs_code,
         text_general_code as description,
-        lat as LAT,
-        lng as LON,
-    FROM read_csv_auto('philly_clean.csv')
+        CLOSEST_STATION_NAME as CLOSEST_STATION,
+        CLOSEST_STATION_OBJECTID,
+        DISTANCE_METERS
+    FROM read_csv_auto('philly_cleaned.csv')
     WHERE YEAR(dispatch_date_time::DATE) = 2020; -- Filter for Philly
